ce020478951589d515e68b01fec3e6d21a2cb467
Nero Duracak <nduracak@nduracak.local>
2024-10-26 11:22:06 -0400

    version 2

--- Components/GridCards/GridCards.jsx
+++ Components/GridCards/GridCards.jsx
@@ -1,18 +1,26 @@
-
 import "./GridCards.css"
-import Download from "./download.jpeg"
-
+import carArray from "/Users/nduracak/P1/public/Array.js"
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import { useEffect, useState } from "react";
 
-export default function gridCards() {
+  export default function GridCards() {
+  const [cars,setCars] = useState([])
+
+  useEffect(()=>{
+      setCars(carArray)
+  },[])
+
+  //let random = cars.map((car)=><li key={car.id}>{car.image}</li>)
+  let images = Math.floor(Math.random()*cars.length)
+
   return (
     <Row xs={1} md={1} className="g-4">
       {Array.from({ length: 4 }).map((_, idx) => (
         <Col key={idx}>
           <Card>
-            <Card.Img variant="top" src={Download} />
+            <Card.Img variant="top" src={images} />
             <Card.Body>
               <Card.Title>Card title</Card.Title>
               <Card.Text>
---
+++ public/Array.js
@@ -0,0 +1,33 @@
+import mustang from "/Users/nduracak/P1/untitled folder 3/Ford mustang.jpeg"
+import chevy from "/Users/nduracak/P1/untitled folder 3/Chevy Colorado.jpeg"
+import lykan from "/Users/nduracak/P1/untitled folder 3/Lykan Hypersport.jpeg"
+import camry from "/Users/nduracak/P1/untitled folder 3/Toyota Camry.jpeg"
+import corolla from "/Users/nduracak/P1/untitled folder 3/toyota corolla.jpeg"
+
+export default [
+            {
+                id: 1,
+                "name": "Mustang",
+                image: mustang,
+            },
+            {
+                id: 2,
+                "name": "Lykan Hypersport",
+                image: lykan ,
+            },
+            {
+                id: 3,
+                "name": "Chevy Colorado",
+                image: chevy,
+            },
+            {
+                id: 4,
+                "name": "Toyota Camry",
+                image: camry,
+            },
+            {
+                id: 5,
+                "name": "Toyota Corolla",
+                image: corolla,
+            }
+        ]
